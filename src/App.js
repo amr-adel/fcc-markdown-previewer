cc6a5204c2c1df6738cfc59b2cf0c90dc856c111
Amr A. Abdelmoez <amradel88@live.com>
2018-10-02 17:09:51 +0200

    feat: Layout sidebar

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Markdown Previewer</h1>
+        <header className="sidebar">
           <img src={logo} alt="markdown previewer logo" className='logo' />
+          <h1 className="brand">
+            Markdown previewer
+          </h1>
         </header>
       </div>
     );
