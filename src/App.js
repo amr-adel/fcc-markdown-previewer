8578b18d6b47de17c6864ed60648e09e79f3d24b
Amr A. Abdelmoez <amradel88@live.com>
2018-10-11 23:27:18 +0200

    fix: Update placeholder markdown

--- src/App.js
+++ src/App.js
@@ -5,51 +5,61 @@ import './App.css';
 class App extends Component {
 
   state = {
-    rawInput: `# Welcome to my React Markdown Previewer!
+    rawInput: `
+# Markdown Previewer
 
-## This is a sub-heading...
-### And here's some other cool stuff:
-  
-Heres some code, \`<div></div>\`, between 2 backticks.
+[![Markdown Previewer](https://raw.githubusercontent.com/amr-adel/fcc-markdown-previewer/master/public/demo.jpg "Markdown Previewer")](https://amr-adel.github.io/fcc-markdown-previewer/)
+
+## FreeCodeCamp | Front End Libraries Projects
+
+A *FreeCodeCamp* challenge on using front end libraries to build a markdown previewer with the below user stories to be fulfilled:
+
+> **Required**
+> 1. I can see a \`textarea\` element with a corresponding \`id="editor"\`.
+> 2.  I can see an element with a corresponding \`id="preview"\`.
+> 3. When I enter text into the \`#editor\` element, the \`#preview\` element is updated as I type to display the content of the textarea.
+> 4. When I enter GitHub flavored markdown into the \`#editor\` element, the text is rendered as HTML in the \`#preview\` element as I type (HINT: You don't need to parse Markdown yourself - you can import the Marked library for this: https://cdnjs.com/libraries/marked).
+> 5. When my markdown previewer first loads, the default text in the \`#editor\` field should contain valid markdown that represents at least one of each of the following elements: a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.
+> 6. When my markdown previewer first loads, the default markdown in the \`#editor\` field should be rendered as HTML in the \`#preview\` element.
+
+> **Optional Bonus**
+> 1. When I click a link rendered by my markdown previewer, the link is opened up in a new tab (HINT: read the Marked.js docs for this one!).
+> 2. When I click a link rendered by my markdown previewer, the link is opened up in a new tab (HINT: read the Marked.js docs for this one!).
+
+---
 
 \`\`\`
-// this is multi-line code:
 
-function anotherExample(firstLine, lastLine) {
-  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-    return multiLineCode;
-  }
-}
+// CODE BLOCK SAMPLE
+<!DOCTYPE html>                       
+<html class="no-js" lang="">                           
+<head>                               
+  <meta charset="utf-8">                               
+  <meta http-equiv="x-ua-compatible" content="ie=edge"> 
+    
+    <title></title>              
+                 
+  <meta name="description" content="">                         
+  <meta name="viewport" content="width=device-width, initial- scale=1, shrink-to-fit=no">
+ <link rel="stylesheet" href="css/main.css">
+</head>   
+                       
+ <body>            
+                  
+   <p>Hello world! This is HTML5 Boilerplate.</p>          
+     <script src="js/vendor/modernizr-{{MODERNIZR_VERSION}}.min.js>
+     </script>
+
+ </body>
+</html>
+
 \`\`\`
-  
-You can also make text **bold**... whoa!
-Or _italic_.
-Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossing stuff out~~.
 
-There's also [links](https://www.freecodecamp.com), and
-> Block Quotes!
+---
 
-And if you want to get really crazy, even tables:
-
-Wild Header | Crazy Header | Another Header?
------------- | ------------- | ------------- 
-Your content can | be here, and it | can be here....
-And here. | Okay. | I think we get it.
-
-- And of course there are lists.
-  - Some are bulleted.
-      - With different indentation levels.
-        - That look like this.
-
-
-1. And there are numbererd lists too.
-1. Use just 1s if you want! 
-1. But the list goes on...
-- Even if you use dashes or asterisks.
-* And last but not least, let's not forget embedded images:
-
-![React Logo w/ Text](https://goo.gl/Umyytc)`
+#### More about the challenge [Build a Markdown Previewer](https://learn.freecodecamp.org/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer).
+#### See it in action [HERE](https://amr-adel.github.io/fcc-markdown-previewer/).
+    `
   }
 
   onChange(raw) {
